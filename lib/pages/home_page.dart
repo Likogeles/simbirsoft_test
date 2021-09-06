800142f4cf0a6c76f773a559b2710edaeb1017ff
Kirill <Yhhuu@yandex.ru>
2021-09-07 00:41:20 +0400

    Calendar added

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,18 +1,63 @@
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({ Key? key }) : super(key: key);
+class HomePage extends StatefulWidget {
+  //const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  CalendarFormat format = CalendarFormat.month;
+  DateTime selectedDay = DateTime.now();
+  DateTime focusedDay = DateTime.now();
 
   @override
   Widget build(BuildContext context) {
     return Container(
       child: Scaffold(
-        backgroundColor: Colors.white,
-        appBar: AppBar(
-          title: Text('List:'),
-          centerTitle: true,
-        ),
-      ),
+          backgroundColor: Colors.white,
+          appBar: AppBar(
+            title: Text('List:'),
+            centerTitle: true,
+          ),
+          body: Column(
+            children: [
+              TableCalendar(
+                focusedDay: focusedDay,
+                firstDay: DateTime(1990),
+                lastDay: DateTime(2077),
+                calendarFormat: format,
+                calendarStyle: CalendarStyle(
+                    isTodayHighlighted: true,
+                    selectedDecoration: BoxDecoration(
+                      color: Colors.blue,
+                      shape: BoxShape.circle,
+                    ),
+                    selectedTextStyle: TextStyle(color: Colors.white)),
+                headerStyle: HeaderStyle(
+                  titleCentered: true,
+                  formatButtonShowsNext: false,
+                ),
+                onFormatChanged: (CalendarFormat _format) {
+                  setState(() {
+                    format = _format;
+                  });
+                },
+                startingDayOfWeek: StartingDayOfWeek.monday,
+                onDaySelected: (DateTime selectDay, DateTime focusDay) {
+                  setState(() {
+                    selectedDay = selectDay;
+                    focusedDay = focusDay;
+                  });
+                },
+                selectedDayPredicate: (DateTime date) {
+                  return isSameDay(selectedDay, date);
+                },
+              ),
+            ],
+          )),
     );
   }
 }
